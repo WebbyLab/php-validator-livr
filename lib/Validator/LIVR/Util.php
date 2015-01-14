efd5bd2c1a3e06b9f111c4c253b10b3059575971
Dan <SDSWanderer@gmail.com>
2015-01-14 02:59:49 +0200

    Extend negative tests with additional format error cases

--- lib/Validator/LIVR/Util.php
+++ lib/Validator/LIVR/Util.php
@@ -12,4 +12,19 @@ class Util {
 
         return array_keys($arr) !== range(0, count($arr) - 1);
     }
+
+    static public function isStringOrNumber($value) {
+
+        if (is_string($value)) {
+            return true;
+        }
+        if (is_int($value)) {
+            return true;
+        }
+        if (is_float($value)) {
+            return true;
+        }
+
+        return false;
+    }
 }
